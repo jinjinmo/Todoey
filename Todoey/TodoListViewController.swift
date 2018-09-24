cf5f1e5ccb1acbe2b25ce6a69f2857a0dfe126bf
jinby <jinmodd@gmail.com>
2018-09-24 15:16:12 +0800

    Data persisted using UserDefaults methods.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,10 +12,17 @@ class TodoListViewController: UITableViewController{
     
     var itemArray = ["Find Eggs","Buy Mike","Find Spoon"]
     
+      let defaults = UserDefaults.standard //相当新建一个默认对象
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       //通过if语句，规避了一些程序上崩溃的bug（也有可能添加的item没有加入数组里面去）
+        if let  items  = defaults.array(forKey: "TodoListArray") as? [String]{
+           
+            itemArray = items
+        }
+ 
 
-    
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -50,16 +57,19 @@ class TodoListViewController: UITableViewController{
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var textFile = UITextField()
+      
         
         //写出警示提醒框
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
-            //通过提醒框所做出相应的反馈（列表数组需要加载一行，同时页面也需要更新一行）
             
-            
+          //通过提醒框所做出相应的反馈（列表数组需要加载一行，同时页面也需要更新一行）
             self.itemArray.append(textFile.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")//对象的直接调用
+          
             self.tableView.reloadData()
             
         }
